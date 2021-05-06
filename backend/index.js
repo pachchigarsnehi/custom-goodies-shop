8ddc008fb2187d88747b1335900f7b014678ccab
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-05 23:38:47 -0700

    category api done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const customer_model = require('./customer_model')
+const customer_model = require("./customer_model")
 const product_model = require("./products_model")
+const category_model = require("./category_model")
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -30,6 +31,9 @@ app.get('/products/category/:id', product_model.getBaseProductsByCategoryID)
 app.post('/products', product_model.createBaseProduct)
 app.put('/products/:id', product_model.updateBaseProduct)
 app.delete('/products/:id', product_model.deleteProductByID)
+
+app.get('/category', category_model.getCategories)
+app.get('/category/:id', category_model.getCategoryByID)
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
