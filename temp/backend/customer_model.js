318dfd14daf4fc22a6d28f8ba47ebb2d6d8e940f
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-09 21:21:06 -0700

    files added

---
+++ temp/backend/customer_model.js
@@ -0,0 +1,70 @@
+const pool = require('./pg_pool').pool
+
+
+const getCustomers = (request, response) => {
+  pool.query('SELECT * FROM customers ORDER BY id ASC', (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    response.status(200).json(results.rows)
+  })
+}
+const getCustomerByID = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM customers WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const updateCustomer = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email, address } = request.body
+
+  pool.query(
+    'UPDATE customers SET name = $1, email = $2, address = $3 WHERE id = $4',
+    [name, email, address, id],
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const createCustomer = (request, response) => {
+  const { name, email, address } = request.body
+  console.log(request.body)
+  console.log(name, address, email)
+  pool.query('INSERT INTO customers (name, address, email) VALUES ($1, $2, $3)  RETURNING *', [name, address, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log(results.rows[0].id)
+    response.status(201).send(`User added with ID: ${results.rows[0].id}, named ${results.rows[0].name}`)
+  })
+}
+
+
+const deleteCustomer = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM customers WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getCustomers,
+  getCustomerByID,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+}
---
+++ temp/backend/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const customer_model = require("./customer_model")
+const product_model = require("./products_model")
+const category_model = require("./category_model")
+const color_model = require("./color_model")
+const font_model = require("./font_model")
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello people!");
+  });
+
+
+app.get('/customers', customer_model.getCustomers)
+app.get('/customers/:id', customer_model.getCustomerByID)
+app.post('/customers', customer_model.createCustomer)
+app.put('/customers/:id', customer_model.updateCustomer)
+app.delete('/customers/:id', customer_model.deleteCustomer)
+
+app.get('/products', product_model.getBaseProducts)
+app.get('/products/:id', product_model.getBaseProductByID)
+app.get('/products/category/:id', product_model.getBaseProductsByCategoryID)
+app.get('/products/color/:id', product_model.getBaseProductsByColorID)
+app.post('/products', product_model.createBaseProduct)
+app.put('/products/:id', product_model.updateBaseProduct)
+app.delete('/products/:id', product_model.deleteProductByID)
+
+app.get('/category', category_model.getCategories)
+app.get('/category/:id', category_model.getCategoryByID)
+
+app.get('/color', color_model.getColors)
+app.get('/color/:id', color_model.getColorByID)
+
+app.get('/fonts', font_model.getFonts)
+app.get('/fonts/:id', font_model.getFontByID)
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
