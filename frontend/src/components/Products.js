bfc6cf0631247fa1ef4e7e9a05cf15143914ccb8
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-13 00:02:41 -0700

    product page grid

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -6,16 +6,66 @@ import Col from "react-bootstrap/Col";
 
 const ShowProducts = () => {
   //use state and hooks
-  const [products, setProducts] = useState([]);
+  const [productsGroup, setProducts] = useState([]);
 
   // function to get and show all products
   const getProducts = async () => {
     try {
       const response = await fetch("http://localhost:8000/products");
-      const jsonData = await response.json();
-
-      //console.log(jsonData);
-      setProducts(jsonData);
+      const products = await response.json();
+      let productGroup = [];
+      for (let i = 0; i + 3 < products.length; i = i + 3) {
+        console.log(products[i], i);
+        if (i === 0 || i % 3 === 0) {
+          productGroup.push(
+            <Row>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={products[i].images[0]} />
+                  <Card.Body>
+                    <Card.Title>{products[i].name}</Card.Title>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>{products[i].price}$</ListGroupItem>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href="#">Buy Now</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={products[i + 1].images[0]} />
+                  <Card.Body>
+                    <Card.Title>{products[i + 1].name}</Card.Title>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>{products[i + 1].price}$</ListGroupItem>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href="#">Buy Now</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={products[i + 2].images[0]} />
+                  <Card.Body>
+                    <Card.Title>{products[i + 2].name}</Card.Title>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>{products[i + 2].price}$</ListGroupItem>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href="#">Buy Now</Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          );
+        }
+      }
+      setProducts(productGroup);
     } catch (err) {
       console.error(err.message);
     }
@@ -25,34 +75,11 @@ const ShowProducts = () => {
     getProducts();
   }, []);
 
-  //console.log(products);
-
   return (
     <Fragment>
-      {products.map(
-        (
-          product // with product object in json
-        ) => (
-          <Container fluid="md">
-            <Row>
-              <Col>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img variant="top" src={product.images[0]} />
-                  <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                  </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>{product.price}$</ListGroupItem>
-                  </ListGroup>
-                  <Card.Body>
-                    <Card.Link href="#">Buy Now</Card.Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        )
-      )}
+      {productsGroup.map((productGroup) => (
+        <Container fluid="md">{productGroup}</Container>
+      ))}
     </Fragment>
   );
 };
