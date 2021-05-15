b5fe5523dd0e40c5eb410acc8aecd0c60074cbae
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-14 21:12:39 -0700

    Revert "Can access diffrent categories from navbar (#9)" (#10)
    This reverts commit e5b5d8539d95782f6cee0187cb2dcc9ff5d47dd6.

--- frontend/src/components/ProductScreen.js
+++ frontend/src/components/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from "react";
 
-import "../components/productscreen.css";
-import "../components/products.css";
-import "../index.css";
+import React from 'react';
+
+
+import '../components/productscreen.css';
+import '../components/products.css';
+import '../index.css';
+
+
+
 
 const SelectProducts = () => {
-  return <div className="main">Product Details</div>;
+ 
+  return(
+    <div className="main">Product Details</div>
+  );
 };
 
-export default SelectProducts;
+  export default SelectProducts;
