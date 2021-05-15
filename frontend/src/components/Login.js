cb71106f14e0fd6a9aa49fab5233e03fb26f2655
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-15 15:32:00 -0700

    login page added

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Button,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 
 const Login = () => {
   return (
-    <Container>
-      <Col>
-        <Row></Row>
-      </Col>
+    <Container
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "auto",
+        marginTop: "125px",
+      }}
+    >
+      <Card className="text-center" style={{ width: "25rem" }}>
+        <Card.Header>Login</Card.Header>
+        <Card.Body>
+          <Card.Title>Enter Account Credentials</Card.Title>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="email">Email</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <br />
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="password">Password</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <Button variant="primary">Log In</Button>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
