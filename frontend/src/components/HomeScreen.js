aef12b9848dfc1afe251721ea374f61ddab03a52
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-14 21:12:15 -0700

    Revert "Can access diffrent categories from navbar (#9)"
    This reverts commit e5b5d8539d95782f6cee0187cb2dcc9ff5d47dd6.

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from "react";
-import "../components/products.css";
-import AllProducts from "./Products";
-import headerHome from "./images/header-home.jpg";
+import React from "react";
+import '../components/products.css';
+import AllProducts from './Products';
 
-// function to get and show all products
-export default function HomeScreen(props) {
-  const [products, setProducts] = useState([]);
 
   // function to get and show all products
-  const getProducts = async () => {
-    try {
-      const response = await fetch("http://localhost:8000/products/");
-      const jsonData = await response.json();
-      console.log(jsonData);
-      setProducts(jsonData);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+  export default function HomeScreen() {
+  
+    return (
 
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  return (
     <div>
-      <img src={headerHome} alt="girl with mask" />
-      <AllProducts products={products} />
+    <AllProducts />
     </div>
-  );
-}
+    )
+};
+
+  
