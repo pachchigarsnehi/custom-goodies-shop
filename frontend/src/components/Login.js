ee91309eed68e0d6d2f65afc3e129be11db83308
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-15 14:46:12 -0700

    footer corrected, login page added

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+
+const Login = () => {
+  return (
+    <Container>
+      <Col>
+        <Row></Row>
+      </Col>
+    </Container>
+  );
+};
+
+export default Login;
