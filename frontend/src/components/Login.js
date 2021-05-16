3222e758a8829a7c73d7017ee1181bc7dddeab2f
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-16 10:57:18 -0700

    Login functionality (#14)
    * login page
    
    * getting user values

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,11 +3,19 @@ import {
   Container,
   Card,
   Button,
+  Form,
+  Row,
+  Col,
   InputGroup,
-  FormControl,
 } from "react-bootstrap";
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target.elements.email.value);
+    console.log(event.target.password.value);
+  };
   return (
     <Container
       style={{
@@ -17,31 +25,54 @@ const Login = () => {
         marginTop: "125px",
       }}
     >
-      <Card className="text-center" style={{ width: "25rem" }}>
+      <Card className="text-center" style={{ width: "35rem" }}>
         <Card.Header>Login</Card.Header>
-        <Card.Body>
-          <Card.Title>Enter Account Credentials</Card.Title>
-          <InputGroup className="mb-3">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="email">Email</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              aria-label="Default"
-              aria-describedby="inputGroup-sizing-default"
-            />
-          </InputGroup>
-          <br />
-          <InputGroup className="mb-3">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="password">Password</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-              aria-label="Default"
-              aria-describedby="inputGroup-sizing-default"
-            />
-          </InputGroup>
-          <Button variant="primary">Log In</Button>
-        </Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group as={Row} controlId="validateEmail">
+            <Form.Label column sm={2}>
+              Email
+            </Form.Label>
+            <Col sm={10}>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter an email
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="validatePassword">
+            <Form.Label column sm={2}>
+              Password
+            </Form.Label>
+            <Col sm={10}>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter password
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Col>
+          </Form.Group>
+          <fieldset></fieldset>
+
+          <Form.Group as={Row}>
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </Form.Group>
+        </Form>
       </Card>
     </Container>
   );
