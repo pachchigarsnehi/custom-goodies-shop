770d0362dcab1e4be66779f12548d4555c9d71a4
Zhou <zhou@zhou-System-Product-Name.lan>
2021-05-19 12:55:40 -0700

    Cart functionality added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from './store'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
+
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
