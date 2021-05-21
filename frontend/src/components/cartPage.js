970ea806fde0d727073cc27f0a3c5eba1e683e6d
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-21 15:35:12 -0700

    updated cart header (#21)
    Co-authored-by: Gabrie <gabriellajeane.deasis.167@my.csun.edu>

--- frontend/src/components/cartPage.js
+++ frontend/src/components/cartPage.js
@@ -17,13 +17,37 @@ export default function CartPage() {
 let total=0
 
 cart.map((item,i)=>{
- let price=parseFloat( item.price)
+ let price=parseFloat( item.price*item.quantity)
  if(!isNaN(price)){
    total+=price
  }
 })
+
   return (
     <div>
+        <div
+            style={{
+              padding: "10px",
+              backgroundColor: "#eee",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems:'center',
+              margin:'20px',
+              borderRadius:'10px'
+              
+            }}
+          >
+            <div style={{ flex: 1 }}>
+              <p style={{margin:'0px'}}>Item</p>
+            </div>
+            <div style={{ flex: 1 }}>
+              <p style={{margin:'0px'}}>Quantity</p>
+            </div>
+            <div style={{padding:'10px'}}>
+              <p style={{margin:'0px'}}>Price</p>{" "}
+            </div> 
+          </div>
       {cart.map((item, i) => {
         return (
           <div
@@ -46,7 +70,7 @@ cart.map((item,i)=>{
               <p style={{margin:'0px'}}>{item.quantity}</p>
             </div>
             <div style={{padding:'10px'}}>
-            <p style={{margin:'0px'}}>$ {item.price}</p>{" "}
+            <p style={{margin:'0px'}}>$ {item.price*item.quantity}</p>{" "}
                </div> 
             <button style={{borderColor:'transparent',backgroundColor:'transparent',color:'red'}} onClick={() => handleDelete(item.id)}>X</button>
           </div>
