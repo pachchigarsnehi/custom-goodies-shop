30e3bcaf747e945f5a9e00695c3a3b14ac5b1ac8
Snehi Pachchigar <snehiat1997@gmail.com>
2021-05-20 17:52:22 -0700

    allow cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || "8000";
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
